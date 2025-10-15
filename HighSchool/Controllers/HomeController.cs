6809ebb7d9385864cecfc13bbc79d903367b3c4e
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-10-15 11:51:54 +0500

    HighSchool Api Project Initialize

---
+++ HighSchool/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HighSchool.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public ActionResult<string> Get() =>
+            "Hello HighSchool";
+    }
+}
